11cf44ebbb00bf8c6800c9df9607e5f7078709cb
daniablank <danielblank@berkeley.edu>
2021-10-20 16:13:54 -0700

    Implement cadCAD for calculation modeling

--- impactmodeling/calculations/views.py
+++ impactmodeling/calculations/views.py
@@ -3,8 +3,9 @@ from cadCAD.configuration import Experiment
 from cadCAD.engine import ExecutionMode, ExecutionContext, Executor
 from scipy.stats import norm
 import numpy as np
+from impactmodeling.utils import db
 
-TOTAL_RANDOM_SAMPLES = 1000
+TOTAL_RANDOM_SAMPLES = 100000
 
 def index(request):
     return HttpResponse("Hello, world. You're at the calculations index.")
@@ -12,7 +13,7 @@ def index(request):
 def model(request, user, proposal_id, param1, operator, param2):
 
     def generate_mean_stddev(lower_bound, upper_bound):
-        mean = (upper_bound - lower_bound)/2.0
+        mean = lower_bound + ((upper_bound - lower_bound)/2.0)
         stddev = np.sqrt(TOTAL_RANDOM_SAMPLES) * (upper_bound - lower_bound)/3.29
         return mean, stddev
 
@@ -33,29 +34,34 @@ def model(request, user, proposal_id, param1, operator, param2):
     if isinstance(param2, list):
         loc2, scale2 = generate_mean_stddev(param2[0], param2[1])
 
+    print(loc2)
+    print(scale2)
+
     exp = Experiment()
 
     def take_a_sample(_params, substep, sH, s, _input, **kwargs):
         current_avg = s["average_result"]
-        for i in range(TOTAL_RANDOM_SAMPLES):
-            leftValue = param1 if loc1 == None else norm.rvs(loc=loc1, scale=scale1)
-            rightValue = param2 if loc2 == None else norm.rvs(loc=loc2, scale=scale2)
-            if operator == "+":
-                current_avg += (leftValue + rightValue)/TOTAL_RANDOM_SAMPLES
-            elif operator == "-":
-                current_avg += (leftValue - rightValue)/TOTAL_RANDOM_SAMPLES
-            elif operator == "*":
-                current_avg += (leftValue * rightValue)/TOTAL_RANDOM_SAMPLES
-            elif operator == "/":
-                current_avg += (leftValue/ rightValue)/TOTAL_RANDOM_SAMPLES
+        leftValue = param1 if loc1 == None else norm.rvs(loc=loc1, scale=scale1)
+        rightValue = param2 if loc2 == None else norm.rvs(loc=loc2, scale=scale2)
+        if operator == "+":
+            current_avg += (leftValue + rightValue)/TOTAL_RANDOM_SAMPLES
+        elif operator == "-":
+            current_avg += (leftValue - rightValue)/TOTAL_RANDOM_SAMPLES
+        elif operator == "*":
+            current_avg += (leftValue * rightValue)/TOTAL_RANDOM_SAMPLES
+        elif operator == "/":
+            current_avg += (leftValue/ rightValue)/TOTAL_RANDOM_SAMPLES
         return "average_result", current_avg
 
     PSUBs = [{"policies": {}, "variables" : {"s_1": take_a_sample}}]
 
-    exp.append_model(initial_state=init_state,partial_state_update_blocks=PSUBs, policy_ops={}, sim_configs={})
+    exp.append_model(initial_state=init_state,partial_state_update_blocks=PSUBs, policy_ops=[lambda _: None], sim_configs=[{'N': 1, 'T': range(TOTAL_RANDOM_SAMPLES), 'M': []}])
 
     exec_mode = ExecutionMode()
     local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
     simulation = Executor(exec_context=local_mode_ctx, configs=exp.configs)
     raw_system_events, tensor_field, sessions = simulation.execute()
+    final_result = raw_system_events[len(raw_system_events) - 1]['average_result']
+    db.models.update_one({"user": user, "proposalId": proposal_id}, {"user": user, "proposalId": proposal_id, "score": final_result}, True)
 
+model(None, "alice", "sample_proposal", 1, "+", [10, 11])
