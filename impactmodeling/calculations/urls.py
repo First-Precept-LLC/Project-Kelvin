f1d4d1489090db2da877d5a594fa016bf38dec64
daniablank <danielblank@berkeley.edu>
2021-10-21 20:43:39 -0700

    add Django converter and URLs

--- impactmodeling/calculations/urls.py
+++ impactmodeling/calculations/urls.py
@@ -1,7 +1,27 @@
-from django.urls import path
+from django.urls import path, register_converter
 
 from . import views
 
+
+class DistributionConverter:
+    regex = "[0-9],[0-9]"
+
+    def to_python(self, value):
+        numbers = value.split(",")
+        return [int(numbers[0]), int(numbers[1])]
+
+    def to_url(self, value):
+        if not isinstance(value, list) or len(value) != 2 or not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise ValueError
+        return str(value[0]) + "," + str(value[1])
+
+register_converter(DistributionConverter, "dist")
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:user>/<str:proposal_id>/<int:param1>/<str:operator>/<int:param2>', views.model),
+    path('<str:user>/<str:proposal_id>/<dist:param1>/<str:operator>/<int:param2>', views.model),
+    path('<str:user>/<str:proposal_id>/<dist:param1>/<str:operator>/<dist:param2>', views.model),
+    path('<str:user>/<str:proposal_id>/<int:param1>/<str:operator>/<dist:param2>', views.model)
+
 ]
