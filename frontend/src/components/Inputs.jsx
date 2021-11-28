2a107e95cce2c9f1c514660dc132d8c8e160410c
saidutt chakka <saidutt@gmail.com>
2021-11-28 23:26:53 +0530

    impact flow style fixes, favicon updated

--- frontend/src/components/Inputs.jsx
+++ frontend/src/components/Inputs.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import axios from "axios";
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
 import ReactDOM from "react-dom";
-import Header from './Header';
+import Header from "./Header";
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,Redirect,
-  Link
+  Route,
+  Redirect,
+  Link,
 } from "react-router-dom";
 
 export class inputs extends React.Component {
@@ -24,10 +32,10 @@ export class inputs extends React.Component {
 
     this.state = {};
 
-    this.state = { 
-       formValues: [{ name: "", values : "" }]
+    this.state = {
+      formValues: [{ name: "", values: "" }],
     };
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
 
     console.log(`constructor before trigger`);
 
@@ -41,9 +49,9 @@ export class inputs extends React.Component {
   }
 
   addFormFields() {
-    this.setState(({
-      formValues: [...this.state.formValues, { name: "", values: "" }]
-    }))
+    this.setState({
+      formValues: [...this.state.formValues, { name: "", values: "" }],
+    });
   }
 
   removeFormFields(i) {
@@ -53,139 +61,129 @@ export class inputs extends React.Component {
   }
 
   handleSubmit(event) {
-   // event.preventDefault();
-    localStorage.setItem('impactflow', JSON.stringify(this.state.formValues));
+    // event.preventDefault();
+    localStorage.setItem("impactflow", JSON.stringify(this.state.formValues));
     //alert(JSON.stringify(this.state.formValues));
-     window.location.href = '/calculate'
+    window.location.href = "/calculate";
   }
-
 
   async componentDidMount() {
     console.log("componentDidMount");
     console.log(this.contract, this.currentUser, this.nearConfig, this.wallet);
-    if (this.wallet && this.contract)
-      await this.triggerOAuth();
+    if (this.wallet && this.contract) await this.triggerOAuth();
     // this.forceUpdate()
   }
 
-
-
   handleClick = async () => {
-
-		  this.wallet.requestSignIn(
-      'gkolluri.testnet',
-      'NEAR Guest Book'
-    );
-  }
-
+    this.wallet.requestSignIn("gkolluri.testnet", "NEAR Guest Book");
+  };
 
   handleTimeChange(value) {
     this.state.time = value;
   }
 
-
   handleTemperatureChange(value) {
     this.state.temperature = value;
   }
-
-
 
   handleCapitalChange(value) {
     this.state.capital = value;
   }
 
-
-
   async shouldComponentUpdate() {
     console.log(`shouldComponentUpdate`);
   }
 
-  
-
-
-
-
   render() {
-
     console.log(`render start ${JSON.stringify(this.state.threads)}`);
     return (
-
-
-   
-<div class="bg-white">
-<Header />
-     <section class="container mb-3">
-        <div class="row">
+      <div class="bg-white">
+        <Header />
+        <section class="container mb-3">
+          <div class="row">
             <div class="col">
-                <nav class="nav nav-pills nav-justified">
-                    <a class="nav-link active" aria-current="page" href="#">Impact Flow</a>
-                    <a class="nav-link" href="/convo">Impact Discussion</a>
-                </nav>
+              <nav class="nav nav-pills nav-justified">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Impact Flow
+                </a>
+                <a class="nav-link" href="/convo">
+                  Impact Discussion
+                </a>
+              </nav>
             </div>
-        </div>
-    </section>
-    <section class="container text-center ">
-        <div class="row mb-3">
+          </div>
+        </section>
+        <section class="container text-center ">
+          <div class="row mb-3">
             <div class="col">
-                <h3 class="text-color">Inputs - Step 1</h3>
+              <h3 class="text-color">Inputs - Step 1</h3>
             </div>
-        </div>
+          </div>
 
-       <form  onSubmit={this.handleSubmit}>
-          {this.state.formValues.map((element, index) => (
-
-
-
-
-            <div className="form-inline" key={index}>
-                    <div class="row">
-            <div class="col my-block m-2 px-4 flex-column align-items-start">
-      
-                <div class="d-flex justify-content-between w-100 mb-2  align-items-center  flex-row">
-                    <h6>Name</h6>
-                    <p><input  name="name" onChange={e => this.handleChange(index, e)}  type="text" placeholder="Variable Name" /></p>
-
+          <form onSubmit={this.handleSubmit}>
+            {this.state.formValues.map((element, index) => (
+              <div className="form-inline" key={index}>
+                <div class="row">
+                  <div class="col my-block m-2 px-4 flex-column align-items-start">
+                    <div class="d-flex justify-content-between w-100 mb-2  align-items-center  flex-row">
+                      <h6>Name</h6>
+                      <p>
+                        <input
+                          name="name"
+                          onChange={(e) => this.handleChange(index, e)}
+                          type="text"
+                          placeholder="Variable Name"
+                        />
+                      </p>
+                    </div>
+                    <div class="d-flex justify-content-between w-100 mb-2  align-items-center  flex-row">
+                      <h6>Values</h6>
+                      <p>
+                        <input
+                          name="values"
+                          onChange={(e) => this.handleChange(index, e)}
+                          type="text"
+                          placeholder="Comma Seperated"
+                        />
+                      </p>
+                    </div>
+                    <button
+                      class="btn btn-primary w-100 btn-color"
+                      onClick={() => this.removeFormFields(index)}
+                      type="button"
+                    >
+                      {" "}
+                      Remove
+                    </button>
+                  </div>
                 </div>
-                 <div class="d-flex justify-content-between w-100 mb-2  align-items-center  flex-row">
-                    <h6>Values</h6>
-                    <p><input  name="values" onChange={e => this.handleChange(index, e)} type="text" placeholder="Comma Seperated" /></p>
+              </div>
+            ))}
+          </form>
 
-                </div>
-            </div>
-            <button class="btn btn-primary btn-color" onClick={() => this.removeFormFields(index)}  type="button"> Remove</button>
-
-        </div>
-
-            </div>
-
-
-
-
-          ))}
-   
-      </form>
-
-        <div class="d-grid gap-2 mt-4">
-          <button onClick={() => this.addFormFields()} type="button" class="btn btn-primary btn-color" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Add
-              </button>
-            <button class="btn btn-primary btn-color" onClick={() => this.handleSubmit()} type="submit"> Next</button>
-        </div>
-    </section>
-
-
-
- </div>
-    )
+          <div class="d-grid gap-2 mt-4">
+            <button
+              onClick={() => this.addFormFields()}
+              type="button"
+              class="btn btn-primary btn-color"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              Add
+            </button>
+            <button
+              class="btn btn-primary btn-color"
+              onClick={() => this.handleSubmit()}
+              type="submit"
+            >
+              {" "}
+              Next
+            </button>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 
 export default inputs;
-
-
-
-
-
-
-
-
